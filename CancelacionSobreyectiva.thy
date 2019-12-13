48fc128bff9c93c002a87dcbed5dc54939819099
carnunfer <carnunfer@alum.us.es>
2019-12-13 13:14:22 +0100

    Terminada sobreyectividad excepto la última demostración

--- CancelacionSobreyectiva.thy
+++ CancelacionSobreyectiva.thy
@@ -12,80 +12,87 @@ text \<open>\comentario{Hacer demostraciones detalladas.}\<close>
 
 text \<open>\comentario{Añadir lemas usados al Soporte.}\<close>
 
+section \<open>Demostración en Lenguaje natural \<close>
 text \<open>
 El siguiente teorema prueba una caracterización de las funciones
- sobreyectivas, en otras palabras, las funciones sobreyectivas son
- epimorfismos en la categoría de conjuntos. Donde un epimorfismo es un
- homomorfismo sobreyectivo y la categoría de conjuntos es la categoría
- donde los objetos son conjuntos.
+ sobreyectivas. Primero se definirá el significado de la sobreyectividad
+de una función y de la propiedad de ser cancelativa por la derecha. \\
+Una función $f : A \longrightarrow B$ es sobreyectiva si 
+$$\forall y \in B : \exists x \in A : f(x) = y$$
+Una función $f : A \longrightarrow B$ tiene la propiedad de ser
+ canletiva por la izquierda si: 
+$$\forall C : (\forall g,h: B \longrightarrow C) : g \circ f = h \circ f
+\Longrightarrow g = h$$
 
+Luego el teorema es el siguiente: 
 
 \begin {teorema}
   f es sobreyectiva si y solo si  para todas funciones g y h tal que 
 $g \circ f  = h \circ f$ se tiene que g = h.
 \end {teorema}
  
-El teorema lo podemos dividir en dos lemas, ya que el teorema se
- demuestra por una doble implicación, luego vamos a dividir el teorema
- en las dos implicaciones.
+El teorema se puede dividir en dos lemas, ya que  se
+ demuestra por una doble implicación.
 
-\begin {lema}
-  f es sobreyectiva entonces  para todas funciones g y h tal que 
+\begin {lema}[Condición necesaria]
+ Si $f$ es sobreyectiva entonces  para todas funciones g y h tal que 
 $g \circ f = h \circ f$ se tiene que $g = h$.
 \end {lema}
+
 \begin {demostracion}
-\begin {itemize}
-\item Supongamos que tenemos que $g \circ  f = h \circ f$, queremos
+Supongamos que tenemos que $g \circ  f = h \circ f$, queremos
  probar que $g = h.$ Usando la definición de sobreyectividad
- $(\forall y \in Y,  \exists x | y = f(x))$ y nuestra hipótesis,
+ $(\forall y \in Y,  \exists x \| y = f(x))$ y nuestra hipótesis,
  tenemos que: $$g(y) = g(f(x)) = (g \circ f) (x) = (h \circ f) (x) =
  h(f(x)) = h(y).$$
-\item Supongamos que $g = h$, hay que probar que
- $g \circ f = h \circ f.$ Usando nuestra hipótesis, tenemos que:
-$$ (g \circ f)(x) = g(f(x)) = h(f(x)) = (h \circ f) (x).$$
-\end {itemize}
-(*<*).(*>*)
 \end {demostracion}
 
-\begin {lema}
+\begin {lema}[Condición necesaria] 
  Si  para todas funciones g y h tal que $g \circ f  = h \circ f$ se 
 tiene que g = h entonces f es sobreyectiva.
 \end {lema}
 
 \begin {demostracion}
-Para la demostración del ejercicios, primero debemos señalar los
- dominios y codominios de las funciones que vamos a usar.
- $f : C \longrightarrow A,$ $g,h: A \longrightarrow B.$ También debemos
- notar que nuestro conjunto  $B$ tiene que tener almenos dos elementos
- diferentes, supongamos que $B = \{a,b\}.$ \\
-La prueba la vamos a realizar por reducción al absurdo. Luego supongamos
+Para la demostración del lema, primero se debe señalar los
+ dominios y codominios de las funciones que se van a usar.
+ $f : C \longrightarrow A,$ $g,h: A \longrightarrow B.$ También se debe
+ notar que el conjunto  $B$ tiene que tener almenos dos elementos
+ diferentes,luego supongamos que $B = \{a,b\}.$ \\
+La prueba se va a realizar por reducción al absurdo. Luego supongamos
 que nuestra función $f$ no es sobreyectiva, es decir, $\exists y_{1} \in
  A \ @{text " tal que "} \  \nexists x \in C \ : f(x) = y.$ \\
 Definamos ahora las funciones $g,h:$
 $$g(y) = a \  \forall y \in A$$
-$$h(y) = a  \ @{text " si "} \  y \neq y_{1} \ h(y) =  b \ 
- @{text " si "} \  y =  y_{1}$$
-
-Entonces sabemos que $g(y) \neq h(y)  \forall y \in A.$ Sin embargo,
- por hipótesis tenemos que si $g \circ f = h \circ f$, lo cual es
- cierto, se tiene que $h = g.$ Por lo que hemos llegado a una
- contradicción, entonces $f$ es sobreyectiva.
-\end {demostracion}
 
 
-Su especificación es la siguiente, que la dividiremos en dos al igual que 
-en la demostración a mano: 
+$$h(y)= \left\{ \begin{array}{lcc}
+             a &   si  & y \neq y_1 \\
+             b &  si & y = y_1
+             \end{array}
+   \right.$$
+
+
+Entonces $g(y) \neq h(y).$ Sin embargo,
+ por hipótesis se tiene  que si $g \circ f = h \circ f$, lo cual es
+ cierto, entonces $h = g.$ Por lo que hemos llegado a una
+ contradicción, por lo tanto, $f$ es sobreyectiva.
+\end {demostracion} 
 \<close>
+section \<open>Especificación en Isabelle/Hol \<close>
 
-theorem
+text \<open>
+Su especificación es la siguiente, que se dividira en dos al igual que 
+en la demostración a mano: \<close>
+
+theorem caracterizacion_funciones_sobreyectivas:
  "surj f \<longleftrightarrow> (\<forall>g h.(g \<circ> f = h \<circ> f) \<longrightarrow> (g = h))"
   oops
 
-lemma 
+lemma condicion_suficiente:
 "surj f \<Longrightarrow>  (\<forall>g h. (g \<circ> f = h \<circ> f) \<longrightarrow> (g = h))"
   oops
 
-lemma 
+lemma condicion_necesaria:
 "\<forall>g h. (g \<circ> f = h \<circ> f \<longrightarrow> g = h) \<longrightarrow> surj f"
   oops
 
@@ -93,8 +100,7 @@ lemma
   text \<open>
 En la especificación anterior, @{term "surj f"} es una abreviatura de 
   @{text "range f = UNIV"}, donde @{term "range f"} es el rango o imagen
-de la función f.
- Por otra parte, @{term UNIV} es el conjunto universal definido en la 
+de la función f y @{term UNIV} es el conjunto universal definido en la 
   teoría \href{http://bit.ly/2XtHCW6}{Set.thy} como una abreviatura de 
   @{term top} que, a su vez está definido en la teoría 
   \href{http://bit.ly/2Xyj9Pe}{Orderings.thy} mediante la siguiente
@@ -108,16 +114,20 @@ Además queda añadir que la teoría donde se encuentra definido
  teoría contiene la definicion @{term" surj_def"}.
  \begin{itemize}
     \item[] @{thm[mode=Rule] surj_def[no_vars]}
- \hfill (@{text inj_on_def})
+ \hfill (@{text surj__def})
   \end{itemize} 
 
+\<close>
+
+section \<open>Demostración estructurada \<close>
+
+text \<open>
 Presentaremos distintas demostraciones de los lemas. Las primeras son
  las detalladas:
 \<close>
-(*<*)declare [[show_types]]
-(*>*)
 
-lemma sobreyectivadetallada:
+
+lemma condicion_suficiente_detallada:
   assumes "surj f" 
   shows "\<forall>g h. ( g \<circ> f = h \<circ> f ) \<longrightarrow> (g = h)"
 proof (rule allI)
@@ -145,38 +155,51 @@ proof (rule allI)
 qed
 
 
-lemma sobreyectivadetallada2:
-  fixes f :: "'c \<Rightarrow> 'a" 
-  assumes "\<forall>(g :: 'a \<Rightarrow> 'b) (h :: 'a \<Rightarrow> 'b). ( g \<circ> f = h \<circ> f ) \<longrightarrow> (g = h)"
+text \<open> En la siguiente demostración nos hará falta la introducción de
+ los pequeños lemas que demostraremos a continuación: \<close>
+
+
+lemma auxiliar_1:
+  assumes "\<not>(\<forall>x. P(x))"
+  shows   "\<exists>x. \<not>P(x)"
+using assms
+  by auto
+
+lemma auxiliar_2:
+  assumes "\<not>(\<exists>x. P(x))"
+  shows   "\<forall>x. \<not>P(x)"
+using assms
+by auto
+
+lemma condicion_necesaria_detallada:
+  assumes "\<forall>(g :: 'b \<Rightarrow> 'c) h .(g \<circ> (f :: 'a \<Rightarrow> 'b) = h \<circ> f) \<longrightarrow> (g = h)"
+           "\<exists> (x0::'c) (x1::'c). x0 \<noteq> x1"
+         shows " \<forall> (y::'b). (\<exists> (x:: 'a). f x = y)"
+proof (rule ccontr)
+  assume "\<not> (\<forall>y :: 'b. \<exists>x :: 'a. f x = y)"
+  hence "\<exists>y :: 'b . \<not> (\<exists>x :: 'a. f x = y)" by (rule auxiliar_1)
+  then obtain y0 where  "\<not> (\<exists>x :: 'a. f x = y0)" by (rule exE)
+  hence "\<forall>x :: 'a. (\<not> (f x = y0))" by (rule auxiliar_2)
+  obtain a0 where " \<exists>(x1::'c). a0 \<noteq> x1" using assms(2) by (rule exE)
+  then obtain a1 where "a0 \<noteq> a1" by (rule exE)
+  let ?g = "(\<lambda>x. a0)  :: 'b \<Rightarrow> 'c"
+  let ?h = "?g(y0:=a1)"
+  have "\<forall>h .(?g \<circ> (f :: 'a \<Rightarrow> 'b) = h \<circ> f) \<longrightarrow> (?g = h)"
+    using assms(1) by (rule allE)
+  hence 1:"(?g \<circ> (f :: 'a \<Rightarrow> 'b) = ?h \<circ> f) \<longrightarrow> (?g = ?h)" by (rule allE)
+  have 2: "(?g \<circ> (f :: 'a \<Rightarrow> 'b) = ?h \<circ> f)"
+    using [[simp_trace]]
+    using \<open>\<nexists>x :: 'a. (f :: 'a \<Rightarrow> 'b) x = (y0 :: 'b)\<close> by auto
+  have "(?g = ?h)" using 1 2 by (rule mp)
+  hence "a0 = a1" by (metis fun_upd_idem_iff)
+  with `a0 \<noteq> a1` show False by (rule notE)
+qed
+
+lemma condicion_necesaria_detallada_2:
+  assumes " \<forall> (y::'b). (\<exists> (x:: 'a). f x = y)"
   shows "surj f"
-proof (rule surjI)
-  assume 1:" \<not> surj f"
-  have " \<not>(\<forall>y. \<exists>x. y = f x)" using 1 by (simp add: surj_def)
-  then have "\<exists>y. \<nexists>x. y = f x" by simp
-  then obtain y1 where "\<nexists>x. y1 = f x" by (rule exE)
-  then have "\<forall>x. y1 \<noteq> f x"  by simp
-  let ?g = "\<lambda>x :: 'a. a :: 'b" 
-  let ?h =" fun_upd ?g y1 (b :: 'b)"
-  have 2:"?g \<circ> f = ?h \<circ> f \<longrightarrow> ?g = ?h" using assms by blast
-  have 3:"?g \<circ> f = ?h \<circ> f" 
-    by (metis (mono_tags, lifting) fun_upd_def \<open>\<nexists>x :: 'c. (y1 :: 'a) =
- (f :: 'c \<Rightarrow> 'a) x\<close> f_inv_into_f fun.map_cong0)
-  have "?g = ?h" using 2 3 by (rule mp)
-  have "?g \<noteq> ?h" 
-  proof 
-    assume 4: "?g = ?h"
-    show False
-    proof -
-      have "?g = fun_upd ?g y1 (b :: 'b)" using 4 by simp
-      also have "... =  (\<lambda>x. if x = y1 then b else ?g x)"  by (simp add:
-fun_upd_def)
-      finally have 5: "?g = (\<lambda>x. if x = y1 then b else ?g x)" by simp
-      show False
-      proof (cases)
-        oops 
-       
-        
-      
+  by (metis assms surj_def)
+    
 text \<open>En la demostración hemos introducido: 
  \begin{itemize}
     \item[] @{thm[mode=Rule] exE[no_vars]} 
@@ -186,32 +209,46 @@ text \<open>En la demostración hemos introducido:
     \item[] @{thm[mode=Proof] iffI[no_vars]} 
       \hfill (@{text iffI})
   \end{itemize} 
+\<close>
 
-La demostración aplicativa es: \<close>
+section \<open>Demostración aplicativa \<close>
 
-lemma "surj f \<Longrightarrow> ((g \<circ> f) = (h \<circ> f) ) \<longrightarrow> (g = h)"
+text \<open>Las demostraciones aplicativas son: \<close>
+
+lemma demostracion_suficiente_aplicativa:
+"surj f \<Longrightarrow> ((g \<circ> f) = (h \<circ> f) ) \<longrightarrow> (g = h)"
   apply (simp add: surj_def fun_eq_iff)
   apply metis
   done
 
-lemma "surj f \<Longrightarrow> ((g \<circ> f) = (h \<circ> f) ) \<longrightarrow>(g = h)"
-  apply (simp add: surj_def fun_eq_iff ) 
-  by metis
-
+lemma demostracion_necesaria_aplicativa:
+  "\<lbrakk>\<forall>(g :: 'b \<Rightarrow> 'c) h .(g \<circ> (f :: 'a \<Rightarrow> 'b) = h \<circ> f) \<longrightarrow> (g = h);
+          \<exists> (x0::'c) (x1::'c). x0 \<noteq> x1\<rbrakk>\<Longrightarrow> surj f"
+  apply (rule surjI)
+  apply (drule condicion_necesaria_detallada)
+   apply simp
+  apply (erule allE)
+  apply (erule exE)+
+  apply (erule Hilbert_Choice.someI)
+  done
 
 text \<open>
-En esta demostración hemos introducido:
+En estas hemos introducido:
  \begin{itemize}
     \item[] @{thm[mode=Rule] fun_eq_iff[no_vars]} 
       \hfill (@{text fun_eq_iff})
-  \end{itemize} 
+     \item[] @{thm[mode=Rule] Hilbert_Choice.someI[no_vars]} 
+      \hfill (@{text Hilbert_Choice.someI})
+\end{itemize}
 \<close>
+section \<open>Demostración teorema \<close>
+text \<open>En consecuencia, la demostración del teorema es \<close>
+
+theorem caracterizacion_funciones_sobreyectivas:
+ "surj f \<longleftrightarrow>  (\<forall>g h.(g \<circ> f = h \<circ> f) \<longrightarrow> (g = h))"
+  oops
   
-    
 
-
-
- 
 (*<*)
 end 
 (*>*)
