7c0b975487f6ae66ffcd529f7d7f663c85bc130a
carnunfer <carnunfer@alum.us.es>
2020-02-19 19:37:37 +0100

    Añadidas reglas hasta sobreyectiva

--- Soporte.thy
+++ Soporte.thy
@@ -28,10 +28,61 @@ text \<open>
     \hfill (@{text mult_Suc}) 
   \item (p. 348) @{thm[mode=Rule] mult_Suc_right}
     \hfill (@{text mult_Suc_right}) 
+ \item (p. 348) @{thm[mode=Rule] mult_0_right}
+    \hfill (@{text mult_0_right}) 
+\end{itemize}
+
+
+\<close>
+
+section \<open>Conjuntos finitos(18) \<close>
+
+subsection \<open>Predicados para conjuntos finitos \<close>
+text \<open>
+\begin{itemize}
+ \item (p. 419) @{thm[mode=Rule] finite_induct}
+    \hfill (@{text finite_induct}) 
 \end{itemize}
 \<close>
 
+section \<open>Las bases de lógica de orden superior (2)\<close>
 
+subsection \<open>Lógica primitiva \<close>
+subsubsection \<open>Axiomas y definiciones básicas \<close>
+text \<open> 
+\begin{itemize}
+ \item (p. 36) @{thm[mode=Rule] mp}
+    \hfill (@{text mp}) 
+ \item (p. 36) @{thm[mode=Rule] impI}
+    \hfill (@{text impI}) 
+ \item (p. 36) @{thm[mode=Rule] ext}
+    \hfill (@{text ext}) 
+\end{itemize}
+\<close>
+subsection \<open> Reglas fundamentales (2.2)\<close>
+
+subsubsection \<open>Reglas de congruencia para aplicaciones \<close>
+text\<open>
+\begin{itemize}
+ \item (p. 37) @{thm[mode=Rule] fun_cong}
+    \hfill (@{text fun_cong}) 
+\end{itemize}
+\<close>
+
+subsubsection \<open>Cuantificadores universales(1) (2.2.5) \<close>
+text \<open>
+\begin{itemize}
+ \item (p. 38) @{thm[mode=Rule] allE}
+    \hfill (@{text allE}) 
+\end{itemize}
+\<close>
+subsubsection \<open>Cuantificadores universales(2) (2.2.12)\<close>
+text\<open>
+\begin{itemize}
+ \item (p. 41) @{thm[mode=Rule] allI}
+    \hfill (@{text allI}) 
+\end{itemize}
+\<close>
 (*<*)
 end
 (*>*) 
