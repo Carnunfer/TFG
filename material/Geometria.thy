d2a0d5a3df162cf196e6b3deaeb5c57da0befae9
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-03-23 09:59:13 +0100

    Revisión del 23-mar-20

--- material/Geometria.thy
+++ material/Geometria.thy
@@ -75,7 +75,8 @@ proof -
     using A5 by auto
   ultimately have "distinct [p1, p2, p3] \<and> {p1, p2, p3} \<subseteq> plane" 
     by auto
-  then show ?thesis by (intro exI)
+  then show ?thesis 
+    by (intro exI)
 qed
 
 (* ---------------------------------------------------------------------  
@@ -86,23 +87,25 @@ lemma (in Simple_Geometry) card_of_plane_greater:
   assumes "finite plane" 
   shows "card plane \<ge> 3"
 proof -
- obtain "p1" "p2" "p3" where 
+  obtain "p1" "p2" "p3" where 
     "distinct [p1, p2, p3] \<and> {p1, p2, p3} \<subseteq> plane"
-   using  three_points_exist by auto
-  moreover then have "{p1, p2, p3} \<subseteq> plane"  by (rule conjE)
-   then have "card {p1, p2, p3} \<le> card plane" 
+    using three_points_exist by auto
+  moreover then have "{p1, p2, p3} \<subseteq> plane"  
+    by (rule conjE)
+  then have "card {p1, p2, p3} \<le> card plane" 
     using assms by (simp add: card_mono)
   ultimately show ?thesis  by auto
 qed
 
+(* --------------------------------------------------------------------- 
+   Problem 19
+   ------------------------------------------------------------------ *)
 
-(*  ----------------------------  *)
-(* |   Problem 19 (2 marks):   | *)
-(*  ----------------------------  *)
-(* GIVE THE SMALLEST MODEL! *)
 definition "plane_3 \<equiv> {1::nat,2,3} "
+
 definition "lines_3 \<equiv> {{1,2},{2,3},{1,3}}"
-interpretation Simple_Geometry_smallest_model: 
+
+interpretation Simple_Geometry_smallest_model:
   Simple_Geometry plane_3 lines_3
   apply standard 
       apply (simp add: plane_3_def)
