7a7090f6213d332f366d047f28df0a7afa40e454
carnunfer <carnunfer@alum.us.es>
2020-04-18 22:27:35 +0200

    Terminado el archivo geometria excepto ultimo paso de interpretation

--- material/Geometria.thy
+++ material/Geometria.thy
@@ -734,12 +734,12 @@ proof -
 
 definition "plane_7 \<equiv> {1::nat,2,3,4,5,6,7} "
 
-definition "lines_7 \<equiv> {{1,2},{2,3},{1,3},{1,4},{4,2},{4,3},{5,1},{5,2},{5,3},{5,4}
-,{6,1},{6,2},{6,3},{6,4},{6,5},{7,1},{7,2},{7,3},{7,4},{7,5},{7,6}}"
+definition "lines_7 \<equiv> {{1,2,3},{1,6,5},{3,4,5},{5,7,2},{3,7,6},{1,4,7},{2,4,6}}"
 
 interpretation Projective_Geometry_smallest_model:
   Projective_Geometry plane_7 lines_7
   apply standard 
+        apply (simp add: plane_7_def lines_7_def)+
   oops
 
 
