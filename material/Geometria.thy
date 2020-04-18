b701e0f61f6f8c8abf9a97ac05c0e79a20f6f576
carnunfer <carnunfer@alum.us.es>
2020-04-18 21:47:50 +0200

    Terminado ejercicio 30

--- material/Geometria.thy
+++ material/Geometria.thy
@@ -419,7 +419,7 @@ proof
     obtain "h" where 6:"h \<in> lines \<and> p \<notin> h" using 1 external_line by auto
     then obtain "a" "b" "c" 
       where  2:"{a,b,c} \<subseteq> plane \<and> distinct [a,b,c] \<and> {a,b,c} \<subseteq> h"
-      using A7' by auto 
+      using A7a  by auto  
     then obtain "l"  where 3:"l \<in> lines \<and> {a,p} \<subseteq> l " using 1 A3 by auto
     obtain "m"  where 4:"m \<in> lines \<and> {b,p} \<subseteq> m " using 1 2 A3 by auto
     obtain "n"  where 5:"n \<in> lines \<and> {c,p} \<subseteq> n " using 1 2 A3 by auto
@@ -493,15 +493,255 @@ qed
 (*  -----------------------------  *)
 (* |   Problem 30 (8 marks):   | *)
 (*  -----------------------------  *)
+
+
 lemma (in Projective_Geometry) at_least_seven_points: 
   "\<exists>p1 p2 p3 p4 p5 p6 p7. distinct [p1,p2,p3,p4,p5,p6,p7] \<and> {p1,p2,p3,p4,p5,p6,p7} \<subseteq> plane" 
+proof -
+  obtain "l" where 1:"l \<in> lines" using one_line_exists by auto
+  then obtain "x" where 2:"card x = 3 \<and> x \<subseteq> l" using A7 by auto
+  then have "card x = 3" by (rule conjE)
+  then obtain "p1" "p2" "p3" where 3: "distinct [p1,p2,p3] \<and> x =
+ {p1,p2,p3}" using construct_set_of_card3  by metis
+  then have 4:"{p1,p2,p3} \<subseteq> l" using 2 by auto
+  then have 6:"{p1,p2,p3} \<subseteq> plane " using A2 1 by auto
+  obtain "q" where 5:"q \<in> plane \<and> q \<notin> l" using A5 1  by auto
+  then have 11:"distinct [p1,p2,p3,q]" using 3 4   by auto
+  obtain "l1" where 7:"l1 \<in> lines \<and> {p1,q} \<subseteq> l1" using 5 6 A3 by auto
+  then have 8:"l1 \<noteq> l" using 5 by auto
+  obtain "p4" where 9:"p4 \<notin> {p1,q} \<and> p4 \<in> l1" using A7b 7 by metis
+  then have 34:"p4 \<in> plane" using A2 7 by auto
+  have 17:"p4 \<noteq> p2" 
+  proof 
+    assume 10:"p4 = p2" 
+     have " l \<inter> l1 = {} \<or> (\<exists>q \<in> plane. l \<inter> l1 = {q}) " using A4 7 1 8 by
+         auto
+     then show False
+     proof 
+       assume "l \<inter> l1 = {}" 
+       then show False using 7 4 by auto
+     next 
+       assume  "\<exists>q\<in>plane. l \<inter> l1 = {q}" 
+       then obtain "t" where "l \<inter> l1 = {t}" by auto
+       then have "{p1,p2} \<subseteq> {t}" using 7 4 9 10 by auto
+       then show False using 11 by auto
+     qed
+   qed
+   have 18:"p4 \<noteq> p3" 
+ proof 
+    assume 10:"p4 = p3" 
+     have " l \<inter> l1 = {} \<or> (\<exists>q \<in> plane. l \<inter> l1 = {q}) " using A4 7 1 8 by
+         auto
+     then show False
+     proof 
+       assume "l \<inter> l1 = {}" 
+       then show False using 7 4 by auto
+     next 
+       assume  "\<exists>q\<in>plane. l \<inter> l1 = {q}" 
+       then obtain "t" where "l \<inter> l1 = {t}" by auto
+       then have "{p1,p3} \<subseteq> {t}" using 7 4 9 10 by auto
+       then show False using 11 by auto
+     qed
+   qed
+   obtain "l2" where 13:"l2 \<in> lines \<and> {p2,q} \<subseteq> l2" using 5 6 A3 by auto
+   then obtain "p5" where 12:"p5 \<notin> {p2,q} \<and> p5 \<in> l2" using A7b 7 by metis
+   then have 35:"p5 \<in> plane" using 13 A2 by auto
+   have 27:"l2 \<noteq> l" using 5 13 by auto
+   have 19:"p5 \<noteq> p1"  
+   proof 
+ assume 10:"p5 = p1" 
+     have " l \<inter> l2 = {} \<or> (\<exists>q \<in> plane. l \<inter> l2 = {q}) " using A4 13 1 27 by
+         auto
+     then show False
+     proof 
+       assume "l \<inter> l2 = {}" 
+       then show False using 13 4 by auto
+     next 
+       assume  "\<exists>q\<in>plane. l \<inter> l2 = {q}" 
+       then obtain "t" where "l \<inter> l2 = {t}" by auto
+       then have "{p1,p2} \<subseteq> {t}" using 10 13 12 4 by auto
+       then show False using 11 by auto
+     qed
+   qed
+   have 20: "p5 \<noteq> p3" 
+  proof 
+ assume 10:"p5 = p3" 
+     have " l \<inter> l2 = {} \<or> (\<exists>q \<in> plane. l \<inter> l2 = {q}) " using A4 13 1 27 by
+         auto
+     then show False
+     proof 
+       assume "l \<inter> l2 = {}" 
+       then show False using 13 4 by auto
+     next 
+       assume  "\<exists>q\<in>plane. l \<inter> l2 = {q}" 
+       then obtain "t" where "l \<inter> l2 = {t}" by auto
+       then have "{p3,p2} \<subseteq> {t}" using 10 13 12 4 by auto
+       then show False using 11 by auto
+     qed
+   qed
+   have 15:"l1 \<noteq> l2" 
+   proof 
+     assume 14:"l1 = l2" 
+     have " l \<inter> l1 = {} \<or> (\<exists>q \<in> plane. l \<inter> l1 = {q}) " using A4 7 1 8 by
+         auto
+     then show False 
+     proof 
+       assume "l \<inter> l1 = {}" 
+       then show False using 7 4 by auto
+     next
+       assume " \<exists>q\<in>plane. l \<inter> l1 = {q}" 
+       then obtain "t" where "l \<inter> l1 = {t}" by auto
+       then have "{p1,p2} \<subseteq> {t}" using 7 13 14 4  by auto
+       then show False using 11 by auto
+     qed
+   qed
+   have 21: "p4 \<noteq> p5" 
+   proof 
+     assume 16:"p4 = p5"
+     have " l2 \<inter> l1 = {} \<or> (\<exists>q \<in> plane. l2 \<inter> l1 = {q}) " using A4 7 15 13  by
+         auto
+     then show False 
+     proof 
+       assume "l2 \<inter> l1 = {}"
+       then show False using 13 7 by auto
+     next
+       assume " \<exists>q\<in>plane. l2 \<inter> l1 = {q}"
+       then obtain "t" where "l2 \<inter> l1 = {t}" by auto
+       then have "{p4,q} = {t}" using 16 13 7 12 9 by auto
+       then show False using 9 by auto
+     qed
+   qed
+   have 29:"distinct [p1,p2,p3,p4,p5,q]" using 11 9 17 18 12 19 20 21 by
+       auto
+   obtain "l3" where 22:"l3 \<in> lines \<and> {p3,q} \<subseteq> l3" using A3 5 6 by auto
+   then obtain "p6" where 26:"p6 \<notin> {p3,q} \<and> p6 \<in> l3" using A7b by metis
+   then have 36:"p6 \<in> plane" using 22 A2 by auto
+   have 23:"l3 \<noteq> l" using 22 5 by auto
+   have 30:"p6 \<noteq> p1" 
+   proof 
+     assume 24:"p6 = p1" 
+     have "  l3 \<inter> l = {} \<or> (\<exists>q \<in> plane. l3\<inter> l = {q}) " using A4 23 22 1  by
+         auto
+     then show False
+     proof 
+       assume "l3 \<inter> l = {}" 
+       then show False using 22 4 by auto
+     next 
+       assume " \<exists>q\<in>plane. l3 \<inter> l = {q}" 
+       then obtain "t" where "l3 \<inter> l = {t}" by auto
+       then have "{p1,p3} = {t}" using 24 4 22 26 by auto
+       then show False using 11 by auto
+     qed
+   qed
+   have 31:"p6 \<noteq> p2"
+   proof 
+   assume 24:"p6 = p2" 
+     have "  l3 \<inter> l = {} \<or> (\<exists>q \<in> plane. l3\<inter> l = {q}) " using A4 23 22 1  by
+         auto
+     then show False
+     proof 
+       assume "l3 \<inter> l = {}" 
+       then show False using 22 4 by auto
+     next 
+       assume " \<exists>q\<in>plane. l3 \<inter> l = {q}" 
+       then obtain "t" where "l3 \<inter> l = {t}" by auto
+       then have "{p2,p3} = {t}" using 24 4 22 26 by auto
+       then show False using 11 by auto
+     qed
+   qed
+   have 25:"l1 \<noteq> l3"
+ proof 
+     assume 14:"l1 = l3" 
+     have " l \<inter> l1 = {} \<or> (\<exists>q \<in> plane. l \<inter> l1 = {q}) " using A4 7 1 8 by
+         auto
+     then show False 
+     proof 
+       assume "l \<inter> l1 = {}" 
+       then show False using 7 4 by auto
+     next
+       assume " \<exists>q\<in>plane. l \<inter> l1 = {q}" 
+       then obtain "t" where "l \<inter> l1 = {t}" by auto
+       then have "{p1,p3} \<subseteq> {t}" using 7 22 14 4  by auto
+       then show False using 11 by auto
+     qed
+   qed
+   have 32:"p6 \<noteq> p4"
+  proof 
+     assume 16:"p6 = p4"
+     have " l3 \<inter> l1 = {} \<or> (\<exists>q \<in> plane. l3 \<inter> l1 = {q}) " using A4 7 25 22  by
+         auto 
+     then show False
+     proof 
+       assume "l3 \<inter> l1 = {}"
+       then show False using 22 7 by auto
+     next
+       assume " \<exists>q\<in>plane. l3 \<inter> l1 = {q}"
+       then obtain "t" where "l3 \<inter> l1 = {t}" by auto
+       then have "{p4,q} = {t}" using 16 9 26 22 7  by auto
+       then show False using 9 by auto
+     qed
+   qed
+   have 28:"l2 \<noteq> l3" 
+   proof 
+  assume 14:"l2 = l3" 
+     have " l \<inter> l2 = {} \<or> (\<exists>q \<in> plane. l \<inter> l2 = {q}) " using A4 27 1 13 by
+         auto
+     then show False 
+     proof 
+       assume "l \<inter> l2 = {}" 
+       then show False using 13 4 by auto
+     next
+       assume " \<exists>q\<in>plane. l \<inter> l2 = {q}" 
+       then obtain "t" where "l \<inter> l2 = {t}" by auto
+       then have "{p2,p3} \<subseteq> {t}" using 13 22 14 4  by auto
+       then show False using 11 by auto
+     qed
+   qed
+   have 33:"p6 \<noteq> p5" 
+   proof 
+ assume 16:"p6 = p5"
+     have " l3 \<inter> l2 = {} \<or> (\<exists>q \<in> plane. l3 \<inter> l2 = {q}) " using A4 28 22 13  by
+         auto 
+     then show False
+     proof 
+       assume "l3 \<inter> l2 = {}"
+       then show False using 22 13 by auto
+     next
+       assume " \<exists>q\<in>plane. l3 \<inter> l2 = {q}"
+       then obtain "t" where "l3 \<inter> l2 = {t}" by auto
+       then have "{p5,q} = {t}" using 16 12 26 22 13  by auto
+       then show False using 29  by auto
+     qed
+   qed
+   have 37:"distinct [p1,p2,p3,p4,p5,p6,q]" using 29 26 30 31 32 33 by auto
+   have "{p1,p2,p3,p4,p5,p6,q} \<subseteq> plane" using 6 5 34 35 36 by auto
+   then show ?thesis using 37 by metis
+ qed
 
-  oops
+
+
+
+
+
+
+
+
 (*  -----------------------------  *)
 (* |   Problem 31 (3 marks):    | *)
 (*  -----------------------------  *)
 (*  Give a model of Projective Geometry with 7 points; use the 
     command "interpretation" to prove that it is indeed a model. *)
+
+definition "plane_7 \<equiv> {1::nat,2,3,4,5,6,7} "
+
+definition "lines_7 \<equiv> {{1,2},{2,3},{1,3},{1,4},{4,2},{4,3},{5,1},{5,2},{5,3},{5,4}
+,{6,1},{6,2},{6,3},{6,4},{6,5},{7,1},{7,2},{7,3},{7,4},{7,5},{7,6}}"
+
+interpretation Projective_Geometry_smallest_model:
+  Projective_Geometry plane_7 lines_7
+  apply standard 
+  oops
+
 
  (*  FILL THIS BLANK *)
 
