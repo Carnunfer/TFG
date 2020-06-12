8c31d7fdca9a1491c9b2c68f277f95e00654d104
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-06-12 06:59:02 +0200

    Revisión 12-Jun-20

--- Resumen.thy
+++ Resumen.thy
@@ -6,18 +6,14 @@ begin
 
 text\<open>
 
-La finalidad de este trabajo es la formalización de
- teoremas de diferentes teorías de las matemáticas. Para ello, se ha
- elegido una serie de teoremas de la teoría de números, teoría de
- conjuntos, teoría de funciones, teoría de retículos y teoría de
- geometría. Una vez formalizados los mismos en el sistema
-de pruebas automáticas Isabelle/HOL, se han utilizado teorías ya
- predefinidas en él  y comprobado la similitud existente entre la 
-demostración  en lenguaje natural y la demostración formal. 
-
-\<close>
-
-
+  La finalidad de este trabajo es la formalización de teoremas de
+  diferentes teorías de las matemáticas. Para ello, se ha elegido una
+  serie de teoremas de la teoría de números, teoría de conjuntos, teoría
+  de funciones, teoría de retículos y teoría de geometría. Una vez
+  formalizados los mismos en el sistema de pruebas automáticas
+  Isabelle/HOL, se han utilizado teorías ya predefinidas en él y
+  comprobado la similitud existente entre la demostración en lenguaje
+  natural y la demostración formal.  \<close>
 
 (*<*)
 end
