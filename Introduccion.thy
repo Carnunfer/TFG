dc62c995e9af56ea4a94737b872528070b0dd7fa
carnunfer <carnunfer@alum.us.es>
2020-06-13 20:33:06 +0200

    Corregidos fallos introduccion y añadido abstract

--- Introduccion.thy
+++ Introduccion.thy
@@ -7,45 +7,46 @@ begin
 
 
 text \<open>
- Actualmente el gran interés de la formalización matemática es la
+ Actualmente el gran interés de la formalización matemática reside en la
  capacidad de la verificación de demostraciones mediante un ordenador.
- Para ello, es necesario poder expresar las definiciones, teoremas y
+ Para ello, se hace preciso poder expresar las definiciones, teoremas y
  pruebas en un lenguaje generado por una gramática que permita verificar
  de forma mecánica las pruebas. También hay que dotar al ordenador de
- una información previa al teorema y junto con una orientación humana se
+ una información previa al teorema de forma que junto con una orientación humana, se
  pueda llegar a validar la demostración de los teoremas. La importancia
  que se le atribuye a la formalización es la capacidad de cálculos y
  razonamientos que puede realizar un ordenador a la vez, incluso
  demostrando teoremas muy difíciles como el teorema de los
- Knaster--Tarski como se verá en el Capítulo 5. En \cite{100theorems}
- podemos encontrar una lista de 100 teoremas formalizados junto con el
+ Knaster--Tarski como se expondrá en el Capítulo 5. En \cite{100theorems}
+ pueden encontrarse una lista de 100 teoremas formalizados junto con el
  programa usado.
 
- Dentro de los sistemas de pruebas automáticas los más usados como se
- puede ver en \cite{Provers} son HOL, Mizar, PVS, Coq, Isabelle/Isar
- entre otros. Isabelle proporciona una estructura genérica para
+ Dentro de los sistemas de pruebas automáticas los más usados, como se
+ aprecia en \cite{Provers} son HOL, Mizar, PVS, Coq, Isabelle/Isar
+ entre otros. Isabelle suministra una estructura genérica para
  construir sistemas deductivos con un especial foco en la prueba de
  teoremas de lógica de orden superior. Sin embargo, Isar proporciona un
  entorno de lenguaje propio, diseñado específicamente para el desarrollo
  de pruebas y teorias. En su conjunto Isabelle/Isar es un marco de
- referencia para el desarrollo formal de documentos matemáticos
- incluyendo una comprobación completa de pruebas, en el que las
- definiciones y pruebas se organizan como teorias.  En nuestro caso el
- sistema de pruebas automático que usaremos es Isabelle/HOL que es una
+ referencia para el desarrollo formal de documentos matemáticos,
+ incluida una comprobación completa de pruebas, en el que las
+ definiciones y pruebas se organizan como teorias.  En el presente
+ análisis, el
+ sistema de pruebas automático utilizado es Isabelle/HOL que es una
  especialización de Isabelle/Isar con lógica de orden superior(HOL).
 
  El objetivo general de este trabajo es mostrar como se elaboran
- demostraciones formales y estructuradas en Isabelle/HOL. Mostrando la
+ demostraciones formales y estructuradas en Isabelle/HOL, manifestando la
  capacidad que tiene este sistema de pruebas automáticas en los
  diferentes aspectos de las matemáticas.
 
- El objetivo específico es estudiar la similitud que se encuentran entre
+ El objetivo específico es estudiar la similitud que hallada entre
  las demostraciones en lenguaje natural y las de lenguaje formal en
  Isabelle/HOL de aspectos básicos de las diferentes teorías de las
  matemáticas.
 
- La metodología utilizada para este trabajo fue seleccionar y probar
- formalmente teoremas en las distintas áreas de las matemáticas de forma
+ La metodología utilizada para este trabajo ha sido seleccionar y probar
+ formalmente teoremas en las distintas áreas de las matemáticas, de forma
  que se muestre la capacidad de Isabelle/HOl en los diferentes ámbitos.
  Una vez escogido el teorema para formalizar se ha seguido siempre el
  mismo esquema:
@@ -59,66 +60,66 @@ text \<open>
 
  Tanto para la demostración formal como para su anterior especificación
  ha sido necesario usar diferentes teorías ya predefinidas en
- Isabelle/HOl e incluso en algunos casos introducir una serie de lemas y
+ Isabelle/HOl, e incluso, en algunos casos introducir una serie de lemas y
  definiciones para poder llegar a entender y demostrar formalmente el
  teorema.
 
- La descripción de los capítulos es la siguiente.
+ \textit{La descripción de los capítulos es la siguiente.}
 
- Dentro de la gran cantidad de aspectos que podemos encontrar en las
- matemáticas, se ha elegido 5 de ellos que son los que vamos a tratar.
+ Dentro de la gran cantidad de teorías matemáticas, se han elegido cinco
+ de ellas con las que trataremos.
 
  En el capítulo 1 se muestran dos teoremas de la teoría de números. El
  primero de ellos es un resultado clásico sobre números impares. La
- decisión de escoger este teorema fue debido a su demostración, ya que
- se realiza por inducción sobre los números naturales; pudiendo mostrar
- el esquema inductivo predefinido en Isabelle y observando la gran
+ decisión de escoger este teorema se debe a su demostración, ya que
+ se realiza por inducción sobre los números naturales, pudiendo mostrar
+ el esquema inductivo predefinido en Isabelle y observar la gran
  similitud existente entre la demostración en lenguaje natural y formal.
- También se muestra la capacidad de Isabelle/HOL dando pruebas menos
+ También se manifiesta la capacidad de Isabelle/HOL dando pruebas menos
  explícitas e incluso automáticas. El segundo teorema es una propiedad
- sobre los conjuntos finitos de números naturales que al igual que en el
- anterior se demuestra de manera inductiva, pero esta vez una inducción
- sobre conjuntos finitos; mostrando también el esquema ya predefinido en
+ sobre los conjuntos finitos de números naturales que, al igual que en el
+ anterior, se demuestra de manera inductiva; pero esta vez una inducción
+ sobre conjuntos finitos, mostrando también el esquema ya predefinido en
  Isabelle/HOL de inducción sobre conjuntos finitos.
 
- En el capítulo 2 podemos encontrar dos resultados de la teoría de
+ En el capítulo 2 se analizan dos resultados de la teoría de
  funciones. Estos son una caracterización sobre funciones inyectivas y
  sobreyectivas respectivamente. La importancía de estos teoremas es
- mostrar la capacidad de Isabelle/HOL a la hora de trabajar con tipos;
+ exponer la capacidad de Isabelle/HOL para trabajar con tipos;
  es decir, con los dominios y codominios de las funciones. En la
- demostración de ambos teoremas es necesario especificar los tipos tanto
- de las funciones como de los elementos, debido a que en el caso de no
- especificarlo Isabelle/HOL toma el tipo más general posible y no te
- admite la prueba. También se ha de descatar la necesidad de usar
+ demostración de ambos teoremas se hace preciso especificar los tipos tanto
+ de las funciones como de los elementos, debido a que, en el caso de no
+ especificarlo, Isabelle/HOL toma el tipo más general posible y no
+ admite la prueba. Se ha de descatar la necesidad de usar
  definiciones de la teoría ya predefinida
  \href{http://bit.ly/2XuPQx5}{Fun.thy} e introducir unas nuevas para
  conseguir la similitud con la especificación y demostración formal.
 
  En el capítulo 3 se muestra el teorema de Cantor, un teorema importante
- de la teoría de conjuntos. La elección del teorema fue debido a su
- demostración formal y automática, ya que su demostración formal es
- idéntica a la demostración en lenguaje natural y en cuanto a su
- demostración automática cabe destacar la capacidad de Isabelle/HOL para
- demostrarlo automáticamente.
+ de la teoría de conjuntos. La elección del mismo se debe a su
+ demostración formal y automática, ya que está  es
+ idéntica a la demostración en lenguaje natural y, en cuanto a la
+ automática, cabe destacar la capacidad de Isabelle/HOL para realizarlo
+ automáticamente.
 
  En el capítulo 4 se estudia el teorema del punto fijo de Knaster--Tarski
- un resultado de la teoría de retículos. La elección del teorema fue
+ un resultado de la teoría de retículos. La elección de este surge por
  el interés en la teoría de retículos, ya que es una teoría importada en
- Isabelle/HOL y para su perfecta comprensión tiene una notación
- específica. Además notar la peculiaridad de los retículos y de los
+ Isabelle/HOL y para su plena comprensión tiene una notación
+ específica. Además subrayar la peculiaridad de los retículos y de los
  retículos completos que se definen en Isabelle/HOL como clases.
 
- En el capítulo 5 se formaliza la teoría de la geometría, en la que
- diferenciamos tres tipos diferentes: geometría simple, geometría no
+ En el capítulo 5, se formaliza la teoría de la geometría, en la que
+ diferenciamos tres tipos: geometría simple, geometría no
  proyectiva y geometría proyectiva. Cada tipo de geometría la
- declararemos en un entorno local definiendo los axiomas propios de ella
- y dentro de él, demostrando una serie de lemas y dando, por último, una
- interpretación del mínimo modelo que se puede formar verificando los
+ declararemos en un entorno local, definiendo los axiomas propios de ella
+ y, dentro de él, demostrando una serie de lemas y dando, por último, una
+ interpretación del mínimo modelo con que se puede formar verificando los
  axiomas.
 
- Por último cabe destacar el apéndice A, en el que basándonos en el
- \href{http://bit.ly/2OMbjMM}{libro de HOL} mostramos todos las reglas y
- lemas usados en el trabajo agrupándolas en diferentes secciones.
+ Por último, cabe destacar el apéndice A, en el que, basándonos en el
+ \href{http://bit.ly/2OMbjMM}{libro de HOL} se indican todas las reglas y
+ lemas usados en el trabajo agrupadas en diferentes secciones.
 
 \<close>
 
--- Resumen.thy
+++ Resumen.thy
@@ -7,7 +7,7 @@ begin
 text\<open>
 
 La finalidad de este trabajo es la formalización de
- teoremas de diferentes teorías de las matemáticas. Para ello, se ha
+ teoremas de diferentes teorías de las matemáticas. Para ello, se han
  elegido una serie de teoremas de la teoría de números, teoría de
  conjuntos, teoría de funciones, teoría de retículos y teoría de
  geometría. Una vez formalizados los mismos en el sistema
@@ -17,6 +17,17 @@ demostración  en lenguaje natural y la demostración formal.
 
 \<close>
 
+section \<open>Abstract \<close>
+
+text \<open>
+The purpose of this project is the formalization of theorems from
+ differents mathematic's theories. For it, some theorems have been
+ choosen from number theory, set theory, function theory, lattice theory
+and geometry theory. Once they have been formalized by the automatic proof system
+ Isabelle/HOl, theories have already been used predefined in it and 
+checking the existing similarity between proof in natural language and
+ formal proof.
+\<close>
 
 
 (*<*)
